d82fbe4a36aa9f63d90fc10d7d3e2f62c95b1b5f
Masataka Kuwabara <kuwabara@pocke.me>
2017-06-28 19:32:56 +0900

    Add test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,16 @@
+require 'tmpdir'
+require 'pathname'
+require 'fileutils'
+
 require 'minitest'
 require 'minitest/autorun'
 
 require 'merge_db_schema'
+
+def mktmpdir(&block)
+  Dir.mktmpdir do |dir|
+    yield Pathname(dir)
+  end
+end
+
+DataDir = Pathname(__dir__) / 'data'
