3700da5c4432a6f3bc86fb1c552cc2ef9030bfe2
Masataka Kuwabara <kuwabara@pocke.me>
2017-07-01 18:48:36 +0900

    Refactor test

--- test/test_main.rb
+++ test/test_main.rb
@@ -4,51 +4,50 @@ class TestMain < Minitest::Test
   def test_main_simple
     mktmpdir do |dir|
       original = dir / 'original.rb'
-      current  = dir / 'current.rb'
-      other    = dir / 'other.rb'
-      data_original = DataDir / 'simple/original.rb'
-      data_current  = DataDir / 'simple/patched1.rb'
-      data_other    = DataDir / 'simple/patched2.rb'
-      FileUtils.cp(data_original, original)
-      FileUtils.cp(data_current, current)
-      FileUtils.cp(data_other, other)
+      current  = dir / 'patched1.rb'
+      other    = dir / 'patched2.rb'
+      data_current = DataDir / 'simple/patched1.rb'
+      copy(DataDir / 'simple', dir)
 
       status = MergeDbSchema.main([original.to_s, current.to_s, other.to_s])
 
       assert status == 0
-      assert current.read != data_current.read
-      assert_equal current.read, DataDir.join('simple/expected.rb').read
-
-      assert !data_current.read.match(/version: 20170628094212/)
-      assert current.read.match(/version: 20170628094212/)
-      assert !current.read.match(/\={7,}/)
-      assert !current.read.match(/\<{7,}/)
-      assert !current.read.match(/\>{7,}/)
+      assert_merged(current, data_current, DataDir.join('simple/expected.rb'))
     end
   end
 
   def test_main_simple_reverse
     mktmpdir do |dir|
       original = dir / 'original.rb'
-      current  = dir / 'current.rb'
-      other    = dir / 'other.rb'
-      data_original = DataDir / 'simple/original.rb'
-      data_current  = DataDir / 'simple/patched2.rb'
-      data_other    = DataDir / 'simple/patched1.rb'
-      FileUtils.cp(data_original, original)
-      FileUtils.cp(data_current, current)
-      FileUtils.cp(data_other, other)
+      current  = dir / 'patched2.rb'
+      other    = dir / 'patched1.rb'
+      data_current = DataDir / 'simple/patched2.rb'
+      copy(DataDir / 'simple', dir)
 
       status = MergeDbSchema.main([original.to_s, current.to_s, other.to_s])
 
       assert status == 0
-      assert current.read != data_current.read
-      assert_equal current.read, DataDir.join('simple/expected.rb').read
-
-      assert current.read.match(/version: 20170628094212/)
-      assert !current.read.match(/\={7,}/)
-      assert !current.read.match(/\<{7,}/)
-      assert !current.read.match(/\>{7,}/)
+      assert_merged(current, data_current, DataDir.join('simple/expected.rb'))
     end
+  end
+
+  # @param src [Pathname]
+  # @param dst [Pathname]
+  def copy(src, dst)
+    files = Dir.glob(src / '*')
+    FileUtils.cp files, dst
+  end
+
+  # @param current [Pathname]
+  # @param current_original [Pathname]
+  # @param expected [Pathname]
+  def assert_merged(current, current_original, expected)
+    assert current.read != current_original.read
+    assert_equal current.read, expected.read
+
+    assert current.read.match(/version: 20170628094212/)
+    assert !current.read.match(/\={7,}/)
+    assert !current.read.match(/\<{7,}/)
+    assert !current.read.match(/\>{7,}/)
   end
 end
