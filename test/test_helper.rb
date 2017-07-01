e6ae818a1589c3209bed5c12a59c863456699f00
Masataka Kuwabara <kuwabara@pocke.me>
2017-07-01 15:26:26 +0900

    Add integration test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,4 +13,10 @@ def mktmpdir(&block)
   end
 end
 
+def sh!(*commands)
+  puts commands.join(' ')
+  system(*commands)
+  raise "command execution is failed. status: #{$?.exitstatus}" unless $?.success?
+end
+
 DataDir = Pathname(__dir__) / 'data'
